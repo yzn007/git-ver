ff58ff3a8b25a2bea7047595db6dfce73c8f915c
wallet88 <wallet88@163.com>
2019-03-14 17:39:58 +0800

    target delete

--- bsbProd-beijing/target/bsbProd/public/scripts/banner.js
+++
@@ -1,5 +0,0 @@
-//修改用户密码
-function modifyPassword(){
-    new parent.MyModifyWindowUi().show(window.top);
-}
-
--- bsbProd-beijing/target/bsbProd/public/scripts/dhtmlx/dhtmlxdataprocessor_deprecated.js
+++
@@ -1,8 +0,0 @@
-dataProcessor.prototype.setOnAfterUpdate = function(ev) {
-    this.attachEvent("onAfterUpdate", ev)
-};
-dataProcessor.prototype.enableDebug = function(mode) {
-};
-dataProcessor.prototype.setOnBeforeUpdateHandler = function(func) {
-    this.attachEvent("onBeforeDataSending", func)
-};
